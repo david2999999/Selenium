01941a8e5a52a058e396a9f49aebde194efb6bd2
David Jiang <djiang86@binghamton.edu>
2020-03-21 16:36:53 -0400

    setting up chrome web driver to test on chrome

---
+++ Selenium WebDriver with Java - Basics to Advanced + Frameworks/Section 3 - Brush up Java Concepts for Selenium Automation/Setting up Test with Chrome/ChromeDemo.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// WebDriver is an interface
+// The implementations of WebDriver provides the functionality of all kinds of browsers.
+public class ChromeDemo {
+    public static void main(String[] args) {
+        // create driver object
+        WebDriver driver = new ChromeDriver();
+
+    }
+}
