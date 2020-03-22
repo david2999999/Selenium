421eb4302959143937c288788848fccd2bfd9815
David Jiang <djiang86@binghamton.edu>
2020-03-22 12:58:38 -0400

    querying elements by ID on fb.com

---
+++ Selenium WebDriver with Java - Basics to Advanced + Frameworks/Section 6 - Locator Techniques/Running Tests on Facebook/FacebookTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookTest {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium Chrome Driver\\chromeDriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.fb.com/");
+		driver.findElement(By.id("email")).sendKeys("Email Address");
+		driver.findElement(By.id("pass")).sendKeys("Password");
+	}
+}
