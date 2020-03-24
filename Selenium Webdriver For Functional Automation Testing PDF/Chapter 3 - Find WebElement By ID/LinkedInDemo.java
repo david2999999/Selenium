e2d8faf7d367a540fb01665d2fdbc2e8a87a8daf
David Jiang <djiang@smlny.com>
2020-03-24 14:01:23 -0400

    finding elements by ID and enter text to input field via sendKeys()

---
+++ Selenium Webdriver For Functional Automation Testing PDF/Chapter 3 - Find WebElement By ID/LinkedInDemo.java
@@ -0,0 +1,21 @@
+public class LinkedInDemo {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        driver.get("https://www.linkedin.com/"); // go to LinkedIn home page
+    }
+
+    @AfterTest
+    public void tearDown() throws Exception {
+        driver.quit(); // closes the browser
+    }
+
+    @Test
+    public void joinLinkedIn() {
+        // Find the element with the id 'first-name'
+        // Enter 'Test First Name' via sendKeys
+        driver.findElement(By.id("first-name")).sendKeys("Test First Name");
+    }
+}
