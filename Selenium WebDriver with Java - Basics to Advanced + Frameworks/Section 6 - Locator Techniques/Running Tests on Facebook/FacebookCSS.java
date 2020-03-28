835b6908124cc79e859e5a6faf291a4f609be091
David Jiang <djiang86@binghamton.edu>
2020-03-28 08:28:36 -0400

    facebook css selector test

---
+++ Selenium WebDriver with Java - Basics to Advanced + Frameworks/Section 6 - Locator Techniques/Running Tests on Facebook/FacebookCSS.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookCSS {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\Selenium Chrome Driver\\chromeDriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.fb.com/");
+
+        driver.findElement(By.cssSelector("input[name='email']")).sendKeys("CSS Selectory Email");
+        driver.findElement(By.cssSelector("[value='Log In']")).click();
+    }
+}
