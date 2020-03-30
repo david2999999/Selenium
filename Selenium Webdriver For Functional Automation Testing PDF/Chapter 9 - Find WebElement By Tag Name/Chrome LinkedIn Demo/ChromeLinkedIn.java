cfb34dcca853701ee621711fe905846200d813ec
David Jiang <djiang86@binghamton.edu>
2020-03-30 09:40:48 -0400

    finding web element by tag name

---
+++ Selenium Webdriver For Functional Automation Testing PDF/Chapter 9 - Find WebElement By Tag Name/Chrome LinkedIn Demo/ChromeLinkedIn.java
@@ -0,0 +1,20 @@
+public class ChromeFacebook {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "PATH TO CHROME DRIVER ON DISK");
+        driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com");
+    }
+
+    @AfterTest
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void clickForgotPassword() {
+        driver.findElement(By.tagName("button")).click();
+    }
+}
