36d1fffb0e357713f59d515924360c1c0de2a837
David Jiang <djiang86@binghamton.edu>
2020-04-02 11:12:34 -0400

    locating element by name

---
+++ Selenium WebDriver Practical Guide - PDF/Chapter 1 - Introducing WebDriver and WebElements/WebElement/Locating WebElements/By Locating Mechanism/GoogleSearchButtonByName.java
@@ -0,0 +1,13 @@
+public class GoogleSearchButtonByName {
+    // <button id="gbqfba" aria-label="Google Search" name="btnK"
+    //  class="gbqfba"><span id="gbqfsa">Google Search</span></button>
+    public static void main(String[] args){
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        WebElement searchBox = driver.findElement(By.name("btnK"));
+        searchBox.submit();
+    }
+}
+
+// The locating mechanism used here is 'By.name' and the name is 'btnK'
+
