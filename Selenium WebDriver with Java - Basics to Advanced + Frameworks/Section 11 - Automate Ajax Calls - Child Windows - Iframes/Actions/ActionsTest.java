499d9831612b0e30f9b62938052e10a6e40655f4
David Jiang <djiang86@binghamton.edu>
2020-04-23 11:26:12 -0400

    building compound actions with Actions

--- Selenium WebDriver with Java - Basics to Advanced + Frameworks/Section 11 - Automate Ajax Calls - Child Windows - Iframes/Actions/ActionsTest.java
+++ Selenium WebDriver with Java - Basics to Advanced + Frameworks/Section 11 - Automate Ajax Calls - Child Windows - Iframes/Actions/ActionsTest.java
@@ -1,6 +1,7 @@
 package com.selenium.actions;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,8 +14,17 @@ public class ActionsTest {
         driver.get("https://www.amazon.com/");
 
         WebElement accountAndList = driver.findElement(By.id("nav-link-accountList"));
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
 
         Actions action = new Actions(driver);
-        action.moveToElement(accountAndList).build().perform();;
+        action.moveToElement(searchBox)
+                .click()
+                .keyDown(Keys.SHIFT)
+                .sendKeys("Nintendo DS")
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ENTER)
+                .build().perform();
+
+        // action.moveToElement(accountAndList).build().perform();;
     }
 }
